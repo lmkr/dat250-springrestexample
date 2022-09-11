0c75ad71589543c6072fc8203c66a07735dce967
Lars Kristensen <lmkr@hib.no>
2022-09-11 13:39:50 +0200

    added PUT and POST

--- src/main/java/com/example/demo/TweetController.java
+++ src/main/java/com/example/demo/TweetController.java
@@ -4,39 +4,59 @@ import java.util.Collection;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TweetController {
 
-    private final AtomicLong counter;
     private final TweetDAO tweets;
 
     public TweetController() {
         super();
-        counter = new AtomicLong();
         tweets = new TweetDAO();
     }
 
     @GetMapping("/tweet")
-    public Collection<Tweet> tweet(@RequestParam(value = "name", defaultValue = "World") String name) {
-        // return new Tweet(counter.incrementAndGet(), "lars", "topic", String.format(template, name));
+    public Collection<Tweet> tweet() {
         return tweets.all();
     }
 
     @GetMapping("/tweet/{id}")
-    public Tweet tweet(@PathVariable Long id) {
+    public Tweet getTweet(@PathVariable Long id) {
         // return new Tweet(counter.incrementAndGet(), "lars", "topic", String.format(template, name));
         return tweets.read(id);
     }
 
-    @DeleteMapping("/employees/{id}")
-    void deleteEmployee(@PathVariable Long id) {
+    @PutMapping("/tweet/{id}")
+    Tweet updateTweet(@RequestBody Tweet newTweet, @PathVariable Long id) {
+
+        Tweet tweet = tweets.read(id);
+
+        tweet.setTopic(newTweet.getTopic());
+
+        tweet.setAuthor(newTweet.getAuthor());
+
+        tweet.setMessage(newTweet.getMessage());
+
+        return tweet;
+    }
+
+    @PostMapping("/tweet")
+    Tweet newEmployee(@RequestBody Tweet newTweet) {
+        return tweets.create(newTweet.getAuthor(),newTweet.getTopic(),newTweet.getMessage());
+    }
+
+    @DeleteMapping("/tweet/{id}")
+    void deleteTweet(@PathVariable Long id) {
 
         tweets.delete(id);
     }
+
+
 
 }
